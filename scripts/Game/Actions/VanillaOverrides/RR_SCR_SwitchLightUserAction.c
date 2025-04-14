6f46de1c5471af7dd19d16dfbeacd84e6fe07213
Njpatman <77807058+Njpatman@users.noreply.github.com>
2025-04-14 00:50:54 -0400

    LightSwitches

---
+++ scripts/Game/Actions/VanillaOverrides/RR_SCR_SwitchLightUserAction.c
@@ -0,0 +1,52 @@
+modded class SCR_SwitchLightUserAction : ScriptedUserAction
+{
+	//------------------------------------------------------------------------------------------------
+	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity) 
+	{	
+		if (m_LightComp)
+		{
+			m_LightComp.ToggleLight(!m_LightComp.IsOn());
+			PlaySound(pOwnerEntity, m_LightComp.IsOn());
+		}
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	void PlaySound(IEntity ownerEntity, bool lightsState)
+	{
+		// Sound
+		SoundComponent soundComponent = SoundComponent.Cast(ownerEntity.FindComponent(SoundComponent));
+		if (!soundComponent)
+			return;
+
+		vector offset;
+		if (GetLightAudioPos(offset))
+		{
+			if (lightsState)
+				soundComponent.SoundEventOffset(SCR_SoundEvent.SOUND_FLASHLIGHT_ON, offset);
+			else
+				soundComponent.SoundEventOffset(SCR_SoundEvent.SOUND_FLASHLIGHT_OFF, offset);
+		}
+		else
+		{
+			if (lightsState)
+				soundComponent.SoundEvent(SCR_SoundEvent.SOUND_FLASHLIGHT_ON);
+			else
+				soundComponent.SoundEvent(SCR_SoundEvent.SOUND_FLASHLIGHT_OFF);
+		}
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	bool GetLightAudioPos(out vector pos)
+	{
+		UserActionContext userActionContext = GetActiveContext();
+		if (!userActionContext)
+		{
+			return false;
+		}
+		else
+		{
+			pos = GetOwner().CoordToLocal(userActionContext.GetOrigin());
+			return true;
+		}
+	}
+}
